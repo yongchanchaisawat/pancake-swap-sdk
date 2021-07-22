e525375c3b5863c907035b7ca1d13dc93ec67351
Y4NGK0NG\Y4nGK0nG <yong.c@bitkub.com>
2021-07-22 10:54:54 +0700

    add dist

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | bigint | string
 
 export enum ChainId {
-  MAINNET = 56,
-  TESTNET = 97
+  MAINNET = 96,
+  TESTNET = 25925
 }
 
 export enum TradeType {
@@ -19,7 +19,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS = '0x384e8898B437953257265E65F32201cDF100ae5C'
 
 export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
 
